cafd13e84147e6c94621be4aef36874857372a1d
Ermyas Fekadu <ermyas.fekadu@gmail.com>
2020-11-12 09:15:40 +0300

    Add config for testing

---
+++ src/config/index.js
@@ -0,0 +1,34 @@
+const merge =  require('lodash').merge
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const env = process.env.NODE_ENV
+
+const baseConfig = {
+    port: 5000,
+    db: {
+        url: 'mongodb://localhost:27017/ecommerce-api'
+    },
+    secret: 'daddfesfsfwefwrtrt45656666565656565656'
+}
+
+let envConfig = {}
+
+switch(env){
+    case 'development':
+    case 'dev':
+        envConfig = require('./dev')
+        break;
+    case 'test':
+    case 'testing':
+        envConfig = require('./testing')
+        break;
+    case 'prod':
+    case 'production':
+        envConfig = require('./prod')
+        break;
+    default:
+        envConfig = require('./dev')             
+}
+
+module.exports =  merge(baseConfig, envConfig)
